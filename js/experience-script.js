330c4e30557a1d1b8ceca164a4ca61e0cbd5e248
Pedro Relvas <pedromiguelrelvas@gmail.com>
2023-10-16 00:21:36 +0100

    PMRPF-01 :: refactor :: responsive design improvements on experience page

--- js/experience-script.js
+++ js/experience-script.js
@@ -13,7 +13,6 @@ timelineObserver.observe(document.getElementById('exp-timeline-line'));
 const timelineItemsObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log(entry.target.textContent);
       entry.target.classList.add('exp-timeline-item-animation');
     }
   });
@@ -22,3 +21,28 @@ const timelineItemsObserver = new IntersectionObserver((entries) => {
 document.querySelectorAll('.exp-timeline-item').forEach((element) => {
   timelineItemsObserver.observe(element);
 });
+
+// toggle snake game
+const View = {
+  EXPERIENCE_TIMELINE: 0,
+  SNAKE_GAME: 1
+}
+let view = View.EXPERIENCE_TIMELINE;
+const btnToggleGameView = document.getElementById('btn-experience-toggle-game');
+const elExperienceTime = document.getElementById('experience-timeline');
+const elSnakeSide = document.getElementById('snake-side');
+btnToggleGameView.addEventListener('click', toggleGameView);
+
+function toggleGameView() {
+  if (view === View.EXPERIENCE_TIMELINE) {
+    view = View.SNAKE_GAME;
+    elExperienceTime.style.display = 'none';
+    elSnakeSide.style.display = 'block';
+    btnToggleGameView.style.color = 'white';
+  } else {
+    view = View.EXPERIENCE_TIMELINE;
+    elExperienceTime.style.display = 'block';
+    elSnakeSide.style.display = 'none';
+    btnToggleGameView.style.color = '#2C80FF';
+  }
+}
