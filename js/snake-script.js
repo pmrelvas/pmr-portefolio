db736e46d44ee6720318febb54e6bceebe45c96b
Pedro Relvas <pedromiguelrelvas@gmail.com>
2024-01-23 23:31:05 +0000

    PMRPF-21 :: refactor :: start with a smaller snake

--- js/snake-script.js
+++ js/snake-script.js
@@ -118,8 +118,6 @@ let foodX;
 let foodY;
 let score = 0;
 let snake = [
-  { x: unitSize * 4, y: 0 },
-  { x: unitSize * 3, y: 0 },
   { x: unitSize * 2, y: 0 },
   { x: unitSize, y: 0 },
   { x: 0, y: 0 },
